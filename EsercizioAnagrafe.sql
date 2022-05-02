8b2d9952dae7351f610a0c2749fe65ea27d6655c
DarioZancuo <dario.zancuo@hotmail.it>
2022-05-02 16:15:00 +0200

    esercizio 2 fatto

--- EsercizioAnagrafe.sql
+++ EsercizioAnagrafe.sql
@@ -3,6 +3,12 @@ select città.regione, persona.nome
 from città join persona on città.nome = persona.cittàNascita
 where persona.età >= 18
 
+/* 2 */
+select pf.nome as nome_figlio, pg.nome as nome_genitore
+from persona as pf join genia on pf.nome = genia.figlio
+    join persona as pg on pg.nome = genia.genitore
+where pf.cittàNascita = pg.cittàNascita
+
 /* 3 */
 select distinct persona.nome, persona.cittàNascita
 from persona join genia on persona.nome = genia.genitore
