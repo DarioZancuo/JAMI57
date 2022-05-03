3ac5eef63a187931e04e80adef8c5316a9ed8dab
DarioZancuo <dario.zancuo@hotmail.it>
2022-05-03 11:23:11 +0200

    esercizio 2 fatto

--- EserciziProgrammatori.sql
+++ EserciziProgrammatori.sql
@@ -3,3 +3,10 @@ select autore.codice
 from autore join programmatore on autore.codice = programmatore.codice
     join programma on programma.id = autore.id 
 where programma.anno >= 2000 and programma.linguaggio = "Java"
+
+/* 2 */
+select programmatore.nome, programmatore.categoria
+from autore join programmatore on autore.codice = programmatore.codice
+join programma on programma.id = autore.id
+where programma.linguaggio != "Python"
+order by programmatore.nome
