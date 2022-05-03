8706e1a71790e8da62449bb5b1d5f04e018ce27a
DarioZancuo <dario.zancuo@hotmail.it>
2022-05-03 12:22:36 +0200

    esercizi 4-5 fatti

--- EserciziProgrammatori.sql
+++ EserciziProgrammatori.sql
@@ -16,3 +16,15 @@ select programmatore.nome, autore.codice, programma.anno, programma.linguaggio
 from autore join programmatore on autore.codice = programmatore.codice 
     join programma on programma.id = autore.id
 where programmatore.categoria = 10 and programma.linguaggio != "Java"
+
+/* 4 (errato)*/
+select programmatore.nome, autore.codice, autore.codice, programma.anno
+from autore join programmatore on autore.codice = programmatore.codice
+    join programma on programma.id = autore.id
+where programmatore.codice = autore.codice and programma.linguaggio = "Python"
+
+/* 5 */
+select autore.codice, programmatore.nome
+from autore join programmatore on autore.codice = programmatore.codice
+    join programma on programma.id = autore.id
+where programma.linguaggio = "Java"
